82baa04f395d29163ad111e5c2031194a5e3c3e3
Yati3112 <yatiphoo1996@gmail.com>
2025-10-01 14:34:20 +0700

    Lunch-Site

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,12 @@ module.exports = defineConfig({
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
-		}
+		},
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/Yati-Portfolio/' // This is your repo's name
+    : '/' // This is for local development
+
+
+
   }
 })
